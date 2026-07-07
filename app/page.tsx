fc156d17e128607982fe4b550768b390846b3fb6
Martín Calderón <dev.msco@gmail.com>
2026-07-07 01:01:50 -0700

    feat: add event display to the home page by fetching the events endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,39 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
+import { IEvent } from "@/database";
 
-const Page = () => {
-    return (
-        <section>
-            <h1 className="text-center">
-                The Hub for Every Dev <br /> Event you Can't Miss
-            </h1>
-            <p className="text-center mt-5">
-                Hackatons, Meetups, and Conferences, All in One Place
-            </p>
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-            <ExploreBtn />
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event you Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackatons, Meetups, and Conferences, All in One Place
+      </p>
 
-                <ul className="events list-none">
-                    {events.map((event) => (
-                        <li key={event.title}>
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events list-none">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
